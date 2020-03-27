2c6a9c7304b5398149763b541de6705181ae6243
p21wilson <dev261456>
2020-03-26 23:25:58 -0300

    Frontend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
 import './style.css'
+
+import api from '../../services/api';
 
 import heroesImg from '../../assets/heroes.png';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon(){
+    const [id , setId ] = useState();
+
+    const history = useHistory();
+
+    async function handleAuth(e) {
+        e.preventDefault();
+
+        try{
+            const response = await api.post('auth', { id });
+    
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.name)
+
+            history.push('/profile')
+        }catch(error){
+
+            alert("Opss! Ago deu errado :(, verifique seu ID")
+        }
+
+    }
+  
     return (
        <div className="logon-container">
            <section className="form">
             <img src={logoImg} alt="Be the Hero"/>
-            <form >
+            <form onSubmit={handleAuth}>
                 <h1>Faça seu logon</h1>
-                <input placeholder="Sua ID"/>
+                <input  
+                    value={id}  
+                    onChange={e => setId(e.target.value)}  
+                    placeholder="Sua ID"
+                />
+
                 <button className="button" type="submit">Entrar</button>
                 <Link className="back-link" to="/register">
                     <FiLogIn size={16} color="#E02041"/>
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React , { useState }from 'react';
+import { Link , useHistory} from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 import  './style.css'
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
+
 export default function NewIncident(){
+    
+    const [ title, setTitle ] = useState('');
+    const [ description, setDescription ] = useState('');
+    const [ value, setValue ] = useState('');
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+
+
+    async function handleIncidents(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+
+     try {
+        await api.post('incidents', data, {
+            headers: {
+                Authorization: ongId 
+            }
+        });
+        history.push('/profile');
+     }catch(error){
+        alert("Error no cadastro, por favor tente novamente.");
+     }
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -17,10 +51,24 @@ export default function NewIncident(){
                     Voltar para casos
                 </Link>              
                 </section>
-                <form>
-                <input placeholder="Título do caso"/>
-                <textarea placeholder="Descrição" />
-                <input placeholder="Valor em reais"/>
+                <form onSubmit={handleIncidents}>
+                <input 
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                placeholder="Título do caso"
+                />
+
+                <textarea 
+                 value={description}
+                 onChange={e => setDescription(e.target.value)}
+                placeholder="Descrição" 
+                />
+
+                <input 
+                 value={value}
+                 onChange={e => setValue(e.target.value)}
+                placeholder="Valor em reais"
+                />
             
                 <button className="button" type="submit">Cadastrar</button>
             
